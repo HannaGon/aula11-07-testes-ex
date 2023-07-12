747aa10e09b9b48da38cd9d44163c52f51e6a433
Bruno Hirata <bruno.n.hirata@outlook.com>
2023-07-11 21:03:20 -0300

    Bruno - Exercicio

--- src/test/jest.setup.js
+++ src/test/jest.setup.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 
 const loadEnv = () =>{
     const env = dotenv.config({
-        path: path.resolve(process.env.PWD, '.env')
+        path: path.resolve(process.cwd(), '.env')
     })
     if (env.error) throw error
     process.env=Object.assign(process.env, env.parsed)
